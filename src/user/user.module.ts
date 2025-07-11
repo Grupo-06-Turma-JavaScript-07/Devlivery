c965eb7bf69150ff7242f5b79df4e4285e0cdb19
ddifreju <jufreddi@gmail.com>
2025-07-11 10:36:45 -0300

    Atualizaçõa da app module para prod

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,8 +6,7 @@ import { User } from './entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]),
-    forwardRef(() => AuthModule),],
+    imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
     providers: [UserService],
     controllers: [UserController],
     exports: [UserService],
